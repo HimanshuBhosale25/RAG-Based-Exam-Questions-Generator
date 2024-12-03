107b23db7348da636f74011092e2e49d876d53b7
HIMANSHU Bhosale <himanshubhosale88@gmail.com>
2024-12-04 02:49:24 +0530

    New Branch

--- src/prompt.py
+++ src/prompt.py
@@ -7,13 +7,16 @@ You do this by asking questions about the text below:
 {text}
 ------------
 
-Create 10 single line questions that will prepare for tests.
+Create 10 single line questions that will prepare for tests and do not even add your response.
+The answers to these questions should be within the given context.
+Do not even say anything from your side
 Do not give multiple choice questions.
+Do not add extra new line characters.
+I'm going to split these questions with python so i can get a list of questions.
 Make sure not to lose any important information.
 
 QUESTIONS:
 """
-
 
 refine_template = ("""
 You are an expert at creating practice questions based on  material and documentation.
